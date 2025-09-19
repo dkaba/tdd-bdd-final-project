2128d8eae8a7f425d122bcadd08f610de93e47de
dkaba <david.kabamba@gmail.com>
2025-09-19 16:37:59 +0200

    Exercise 1: Create Fake Products

--- tests/factories.py
+++ tests/factories.py
@@ -19,6 +19,7 @@ Test Factory to make fake objects for testing
 """
 import factory
 from factory.fuzzy import FuzzyChoice, FuzzyDecimal
+from factory import Faker
 from service.models import Product, Category
 
 
@@ -30,5 +31,17 @@ class ProductFactory(factory.Factory):
 
         model = Product
 
-    id = factory.Sequence(lambda n: n)
+    id = factory.Sequence(lambda n: n) # auto-incrementing id
    ## Add code to create Fake Products 
+    name = FuzzyChoice( 
+        choices=[
+        "Hat", "Pants", "Shirt",
+        "Apple", "Banana", "Pots",
+         "Towels", "Ford", "Chevy",
+         "Hammer", "Wrench"])
+    description = Faker("text")
+    price = FuzzyDecimal(0.5, 2000.0, 2)
+    available = FuzzyChoice(choices=[True, False])
+    category = FuzzyChoice(choices=[cat for cat in Category])
+    
+     
