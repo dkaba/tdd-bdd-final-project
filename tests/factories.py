362540da89b4b69f9b3daefd9974903ebe411d23
dkaba <david.kabamba@gmail.com>
2025-09-20 19:19:43 +0200

    Pylint refactoring

--- tests/factories.py
+++ tests/factories.py
@@ -31,17 +31,15 @@ class ProductFactory(factory.Factory):
 
         model = Product
 
-    id = factory.Sequence(lambda n: n) # auto-incrementing id
-   ## Add code to create Fake Products 
-    name = FuzzyChoice( 
+    id = factory.Sequence(lambda n: n)  # auto-incrementing id
+    # Add code to create Fake Products for test
+    name = FuzzyChoice(
         choices=[
-        "Hat", "Pants", "Shirt",
-        "Apple", "Banana", "Pots",
-         "Towels", "Ford", "Chevy",
-         "Hammer", "Wrench"])
+            "Hat", "Pants", "Shirt",
+            "Apple", "Banana", "Pots",
+            "Towels", "Ford", "Chevy",
+            "Hammer", "Wrench"])
     description = Faker("text")
     price = FuzzyDecimal(0.5, 2000.0, 2)
     available = FuzzyChoice(choices=[True, False])
     category = FuzzyChoice(choices=[cat for cat in Category])
-    
-     
